e873e9d5e90210e6cee9efe31649d61e31a0712d
valerieluthi <valerie.luethi92@gmail.com>
2025-04-05 22:30:57 +0200

    updated violin plot

--- analyze.py
+++ analyze.py
@@ -5,15 +5,12 @@ Author:
 Date: 2025-04-05
 Version: 0.1
 """
-import itertools
 import pandas as pd
 import numpy as np
-import scipy.stats as stats
 import matplotlib.pyplot as plt
 import seaborn as sns
 import scikit_posthocs as sp
 from scipy.stats import kruskal
-from statsmodels.stats.multitest import multipletests
 from cleanertransformer import generate_summary_statistics as summary_stats
 
 # load DataFrame
@@ -24,12 +21,9 @@ summary = summary_stats(df,"price_chf")
 print(summary)
 
 # find city with most expensive pizza
-max_price = df.groupby("city")["price_chf"].max().sort_values(ascending=False)
-print(max_price.round(2))
-
-# find city with cheapest pizza
-min_price = df.groupby("city")["price_chf"].min().sort_values(ascending=True)
-print(min_price.round(2))
+city_prices = df.groupby("city")["price_chf"].agg(["max", "min"]).round(2)
+print(city_prices["max"].sort_values(ascending=False))
+print(city_prices["min"].sort_values(ascending=True))
 
 #####
 # compare pizza prices between cities
@@ -47,40 +41,30 @@ print(f"Kruskal_Wallis Test\n p-value: {kruskal_result.pvalue:.6f}")
 # run post-hoc Dunn-Bonferroni test to find which cities differ from each other
 dunn_bonf_result = sp.posthoc_dunn(df, val_col='log_price_chf', group_col='city', p_adjust='bonferroni')
 alpha = 0.05
-sig_results = dunn_bonf_result[dunn_bonf_result < alpha].stack()
-sig_results_cleaned = sig_results.reset_index()
-print(f"Post Hoc Dunn-Bonferroni, significant results (p-value < 0.05): \n {sig_results_cleaned}")
+sig_results = dunn_bonf_result[dunn_bonf_result < alpha].stack().reset_index()
+print(f"Post Hoc Dunn-Bonferroni, significant results (p-value < 0.05): \n {sig_results}")
+# average log prices per city
 city_avg_log_price = df.groupby('city')['log_price_chf'].mean().sort_values(ascending=False)
 print(city_avg_log_price)
 
-# violin plot of prices in different cities
-# set figure size
+# Violin plot for prices in different cities
 plt.figure(figsize=(14, 8))
-# create boxplot with different colors
-city_colours = sns.color_palette("viridis", len(df['city'].unique()))
-# dictionary to map cities to colors
-city_to_colours = {city: city_colours[i] for i, city in enumerate(df['city'].unique())}
-# colour mapping
-color_mapping = df['city'].map(city_to_colours)
-# create violin plot
-ax = sns.violinplot(x="city", y="log_price_chf", data=df,
-                    hue='city', palette=city_to_colours,
-                    split=False, inner="quart", linewidth=1.5, legend=False)
-# overlay individual data points
-sns.stripplot(x="city", y="log_price_chf", data=df, color="black", alpha=0.7,
-              jitter=True, dodge=True)
+sns.violinplot(x="city", y="log_price_chf", data=df, hue='city', split=False, inner="quart", linewidth=1.5, legend=False, palette="viridis")
+# make individual data points visible
+sns.stripplot(x="city", y="log_price_chf", data=df, color="black", alpha=0.7, jitter=True, dodge=True)
 # labels and title
 plt.xlabel("City", fontsize=14)
 plt.ylabel("Pizza Margherita Price (log transformed)", fontsize=14)
 plt.title("Price Distribution of Pizza Margherita across Swiss Cities", fontsize=16, weight='bold')
-# rotate x-axis labels for better readability
+# rotation of x-axis labels for better readability
 plt.xticks(rotation=45, ha='right')
-# add gridlines
+# add gridlines for orientation
 plt.grid(True, axis='y', linestyle='--', alpha=0.7)
 # Show plot
-plt.tight_layout()
+plt.tight_layout() # avoids overlap
 plt.show()
 
+####
 # Test what factors influence a restaurants rating
 
 
