2a68f7b84b21047ce0a7c77b866d47cb33a97d30
valerieluthi <valerie.luethi92@gmail.com>
2025-04-08 16:34:23 +0200

    cleaned up code

--- analyze.py
+++ analyze.py
@@ -5,7 +5,6 @@ Author:
 Date: 2025-04-05
 Version: 0.1
 """
-from cleanertransformer import generate_summary_statistics as summary_stats
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,6 +12,7 @@ import seaborn as sns
 import scikit_posthocs as sp
 import statsmodels.api as sm
 from scipy.stats import kruskal
+from cleanertransformer import generate_summary_statistics as summary_stats
 
 # load DataFrame
 df = pd.read_csv("data/pizza_final.csv", delimiter = ";", header = 0)
@@ -67,39 +67,13 @@ plt.show()
 
 
 ##### Test what factors influence a restaurants rating
-# clean/group column cuisine
-num_rows = df.shape[0] # check number of rows before transformation
-print(num_rows)
-unique_cuisines = df['cuisine'].unique()
-print(unique_cuisines)
-cuisine_groups = {
-    'Italian': ['Italienisch', 'Italienische Pizza', 'Pasta', 'Pizza'],
-    'MiddleEastern': ['Türkisch', 'Döner', 'Kebab', 'Türkische Pizza', 'Pide', 'Balkanküche',
-                'Arabisch', 'Falafel', 'Meze', 'Libanesisch'],
-    'Indian' : ['Indisch'],
-    'Swiss': ['Schweizer Küche'],
-    'Fastfood': ['Snacks', 'Sandwiches', 'Wraps', 'Amerikanisch', 'Burger', 'Hot Dog'],
-    'Other': ['Lebensmittel', 'Alkohol', 'Andere Getränke', 'Lokale Geheimtipps', 'Mittagsangebote', '-25% Deals',
-              'Gourmet']
-}
-
-def group_cuisine(cuisine):
-    for group, cuisines in cuisine_groups.items():
-        if any(cuisine_part in cuisine for cuisine_part in cuisines):
-            return group
-    return 'Other'
-
-df['cuisine_group'] = df['cuisine'].apply(group_cuisine)
-print(df['cuisine_group'].unique())
-print(df['cuisine_group'].value_counts())
-
 # log transform price and min. order value
 df['log_price_chf'] = np.log(df['price_chf'])
 df['log_min_ord_val_chf'] = np.log(df['min_ord_val_chf'])
 # transform num ratings in to categorical variable
 df['num_ratings_cat'] = pd.qcut(df['num_ratings'], q=3, labels=['Low', 'Medium', 'High'])
 # Convert categorical variables into dummies
-df_dummies = pd.get_dummies(df[['num_ratings_cat', 'cuisine_group', 'delivery_fee_chf_cat', 'city']], drop_first=True)
+df_dummies = pd.get_dummies(df[['num_ratings_cat', 'cuisine', 'delivery_fee_chf_cat', 'city']], drop_first=True)
 df_dummies = df_dummies.astype(int)
 # Combine dummies and numeric variables
 var_num = ['log_price_chf', 'log_min_ord_val_chf', 'Median Monthly Wage (CHF)']
